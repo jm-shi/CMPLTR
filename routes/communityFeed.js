afc295f239347de15b5417832ff4dffac73d780d
Jamie S <jamieshi2u@gmail.com>
2019-03-07 22:06:25 -0800

    Link community feed to current routine

--- routes/communityFeed.js
+++ routes/communityFeed.js
@@ -3,17 +3,16 @@ let communityFeed = require('../communityFeed.json');
 /*
  * GET community feed page.
  */
-exports.view = function (req, res) {
+exports.view = function(req, res) {
   res.render('communityFeed', {
     navbarTitle: 'Community Feed',
     communityFeed
-  }); 
+  });
 };
 
-exports.add = function (req, res) {
+exports.add = function(req, res) {
   const id = req.params.id;
   const name = req.params.name;
   const completedDayNumber = req.params.completedDayNumber;
   const routineName = req.params.routineName;
-  
 };
